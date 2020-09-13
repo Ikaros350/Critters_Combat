52ce9528812f6e9ef92fc68ce77f39b0c4ab7a94
Saito Hughes <68065725+Saitohughes@users.noreply.github.com>
2020-09-12 20:10:14 -0400

    fixeo de vainas

--- Assets/Scripts/IPool.cs
+++ Assets/Scripts/IPool.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface IPool 
-{ // esto no se toca, es solo para tomer la cantidad de metodos que tendra la flecha.
-    void Instantiate(); //momento al crear la flecha
-    void Begin(Vector3 position); //momento en que se llama la flecha
+{ // esto no se toca, es solo para tomer la cantidad de metodos que la creacion de critters.
+    void Instantiate(); //momento al crear el critter
+    void Begin(Vector3 position); //momento en que se llama el critter
     
 }
